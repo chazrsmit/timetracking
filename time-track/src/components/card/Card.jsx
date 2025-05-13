3f14be0b2e2880551d54c4330ef2a7fc374de431
Charlotte <smit-charlotte@outlook.com>
2025-05-13 14:07:27 +0200

    update

--- time-track/src/components/card/Card.jsx
+++ time-track/src/components/card/Card.jsx
@@ -21,28 +21,35 @@ export default function Card({ item, period }) {
         ${item.title === "Self Care" ? "self-care" : ""}
         `}></div>
         <div className="card-body">
-            <div className="d-flex justify-content-between">
+          
+            <div className="d-flex justify-content-between align-items-center">
                 <h6>{item.title}</h6>
-                <p className="dots">...</p>
+                <div className="d-flex align-items-center">
+                  <p className="dots">...</p>
+                </div>
             </div>
+
             {period === "daily" && (
               <div>
                 <h1>{item.timeframes.daily.current}hrs</h1>
-                <p>Last week: {item.timeframes.daily.previous}hrs</p>
+                <p className="last">Last week: {item.timeframes.daily.previous}hrs</p>
               </div>
             )}
+
             {period === "weekly" && (
               <div>
                 <h1>{item.timeframes.weekly.current}hrs</h1>
-                <p>Last week: {item.timeframes.weekly.previous}hrs</p>
+                <p className="last">Last week: {item.timeframes.weekly.previous}hrs</p>
               </div>
             )}
+
             {period === "monthly" && (
               <div>
                 <h1>{item.timeframes.monthly.current}hrs</h1>
-                <p>Last week: {item.timeframes.monthly.previous}hrs</p>
+                <p className="last">Last week: {item.timeframes.monthly.previous}hrs</p>
               </div>
             )}
+
             {/* <p className={`${period === "daily"} ? '' : 'none'`}>{item.timeframes.daily.current}hrs (Prev: {item.timeframes.daily.previous}hrs)</p>
             <p className={`${period === "weekly"} ? '' : 'none'`}>{item.timeframes.weekly.current}hrs (Prev: {item.timeframes.weekly.previous}hrs)</p>
             <p className={`${period === "monthly"} ? '' : 'none'`}>{item.timeframes.monthly.current}hrs (Prev: {item.timeframes.monthly.previous}hrs)</p> */}
--- time-track/src/components/user/User.jsx
+++ time-track/src/components/user/User.jsx
@@ -10,12 +10,12 @@ export default function User(props) {
                 <div className="div-img">
                     <img src={img} alt={props.name} />
                 </div>
-                <div>
-                    <p>Report for</p>
-                    <h1>{props.name}</h1>
+                <div className='d-flex flex-column justify-content-center'>
+                    <p className="m-0">Report for</p>
+                    <h1 className="m-0">{props.name}</h1>
                 </div>
             </div>
-            <div className="user-frequency d-flex flex-column gap-3">
+            <div className="user-frequency gap-3">
                 <div onClick={props.handleClick} id="daily" className={`${props.period === "daily" ? "clicked" : ""}`}>Daily</div>
                 <div onClick={props.handleClick} id="weekly" className={`${props.period === "weekly" ? "clicked" : ""}`}>Weekly</div>
                 <div onClick={props.handleClick} id="monthly" className={`${props.period === "monthly" ? "clicked" : ""}`}>Monthly</div>
